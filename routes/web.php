baaa4664e3d31f0a94f85310845ea1fe3f8f8419
Carlos Ferreira <carlos@especializati.com.br>
2023-03-04 11:13:49 -0300

    feat: add two routes, with diferents implementations search elastictsearch

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Product2Controller;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [Product2Controller::class, 'index'])->name('products.index');
+Route::get('/products2', [Product2Controller::class, 'index'])->name('products2.index');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
 Route::get('/', function () {
     return view('welcome');
