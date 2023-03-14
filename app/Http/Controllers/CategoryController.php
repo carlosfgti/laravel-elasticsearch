29e75f16398527b41e334e84bd575054c6f50949
Carlos Ferreira <carlos@especializati.com.br>
2023-03-14 15:05:28 -0300

    feat: create route and controller list categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Core\Category\Application\DTO\ListCategoriesInputDto;
+use Core\Category\Application\UseCases\ListCategoriesUseCase;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function __construct(
+        // protected
+    ) {}
+
+    public function index(Request $request, ListCategoriesUseCase $useCase)
+    {
+        $response = $useCase->execute(
+            input: new ListCategoriesInputDto(
+                filter: $request->filter ?? '',
+                order: $request->get('order', 'DESC'),
+                page: (int) $request->get('page', 1),
+                totalPage: (int) $request->get('totalPerPage', 15),
+            )
+        );
+    }
+}
