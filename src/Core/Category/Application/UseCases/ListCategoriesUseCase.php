cfacbbcc7a90caddd04ea415249a5915bb7edf6e
Carlos Ferreira <carlos@especializati.com.br>
2023-03-15 15:07:04 -0300

    refactor: return array categories

--- src/Core/Category/Application/UseCases/ListCategoriesUseCase.php
+++ src/Core/Category/Application/UseCases/ListCategoriesUseCase.php
@@ -12,25 +12,29 @@ class ListCategoriesUseCase
         protected CategoryRepositoryInterface $repository
     ) {}
 
-    public function execute(ListCategoriesInputDto $input): ListCategoriesOutputDto
+    public function execute(ListCategoriesInputDto $input): array
     {
-        $categories = $this->repository->paginate(
+        return $this->repository->findAll(
             filter: $input->filter,
-            order: $input->order,
-            page: $input->page,
-            totalPage: $input->totalPage,
+            order: $input->order
         );
+        // $categories = $this->repository->paginate(
+        //     filter: $input->filter,
+        //     order: $input->order,
+        //     page: $input->page,
+        //     totalPage: $input->totalPage,
+        // );
 
-        return new ListCategoriesOutputDto(
-            items: $categories->items(),
-            total: $categories->total(),
-            current_page: $categories->currentPage(),
-            last_page: $categories->lastPage(),
-            first_page: $categories->firstPage(),
-            per_page: $categories->perPage(),
-            to: $categories->to(),
-            from: $categories->from(),
-        );
+        // return new ListCategoriesOutputDto(
+        //     items: $categories->items(),
+        //     total: $categories->total(),
+        //     current_page: $categories->currentPage(),
+        //     last_page: $categories->lastPage(),
+        //     first_page: $categories->firstPage(),
+        //     per_page: $categories->perPage(),
+        //     to: $categories->to(),
+        //     from: $categories->from(),
+        // );
 
         // return new ListCategoriesOutputDto(
         //     items: array_map(function ($data) {
